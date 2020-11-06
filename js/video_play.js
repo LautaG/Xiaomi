c47f98327db6de6166acd62ca30f4ebb96213244
LautaG <74028143+LautaG@users.noreply.github.com>
2020-11-05 22:34:10 -0300

    Add files via upload

---
+++ js/video_play.js
@@ -0,0 +1,7 @@
+  window.reproducir = function () {
+            document.getElementById("video_poco").play();
+        };
+
+          function play_video() {
+                     document.getElementById("video_play").style.display = "none";
+        };
